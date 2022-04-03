09413303a435499619017d50c5920c05c941f847
vallarasu <100554866+vallaras@users.noreply.github.com>
2022-04-03 22:02:54 +0530

    Create len1.py

---
+++ len1.py
@@ -0,0 +1,124 @@
+# Python len function
+
+lenexample = ''
+print("\n", lenexample)
+
+# Python Length Example
+print("Length = ", len(lenexample))
+
+# Python len function
+
+stringLenExample = 'Tutorial Gateway'
+print("\n", stringLenExample)
+
+# Python Length of a String Example
+print("Length of a Given String = ", len(stringLenExample))
+
+
+# Python len function
+
+# Python Length of an Empty Tuple
+tupleLen = ()
+print("Length of a Tuple = ", len(tupleLen))
+
+tupleLenExample = (10, 20, 30, 40, 50, 60, 70)
+print("\n", tupleLenExample)
+
+# Python Length of a Tuple Example
+print("Length of a Tuple = ", len(tupleLenExample))
+
+# Python len function
+
+tupleLenExample = ('Banana', 'Orange', 'Blackberry', 'Apple')
+print("\n", tupleLenExample)
+
+# Python Length of Tuple Example
+print("Length of a Tuple = ", len(tupleLenExample))
+
+# Python len function
+
+# Python Length of Empty List
+listLen = ()
+print("Length of a List = ", len(listLen))
+
+# Python Length of List Example 1
+listLenExample = [15, 20, 35, 40, 55, 60, 70]
+print("\n", listLenExample)
+
+print("Length of a List = ", len(listLenExample))
+
+# Python List Length Example 2
+stringListLen = ['apple', 'mango', 'orange', 'cherry','banana']
+print("\n", stringListLen)
+
+print("Length of a List = ", len(stringListLen))
+
+# Python len function
+
+# Python Length of Empty Dictionary
+DictionaryLen = dict()
+print("Length of a Dictionary = ", len(DictionaryLen))
+
+# Python Length of Dictionary Example 1
+DictionaryLenExample = {1: 'mango', 2: 'kiwi', 3: 'cherry'}
+print("\n", DictionaryLenExample)
+
+print("Length of a Dictionary = ", len(DictionaryLenExample))
+
+# Python Dictionary Length Example 2
+mixedDictionaryLen = {'ID': 42, 'name': 'Kevin', 'age': 25, 1: 1200}
+print("\n", mixedDictionaryLen)
+
+print("Length of a Dictionary = ", len(mixedDictionaryLen))
+
+# Python len function
+
+# Python Length of an Empty Set
+SetLen = ()
+print("Length of a Set = ", len(SetLen))
+
+# Python Length of a Set Example 1
+SetLenExample = {125, 200, 350, 400, 505, 607, 760}
+print("\n", SetLenExample)
+
+print("Length of a Set = ", len(SetLenExample))
+
+# Python Set Length Example 2
+stringSetLen = {'apple', 'mango', 'cherry', 'kiwi', 'orange', 'banana'}
+print("\n", stringSetLen)
+
+print("Length of a Set = ", len(stringSetLen))
+
+# Python len function
+
+# Python Length of an Empty Array
+ArrayLen = []
+print("Length of a Array = ", len(ArrayLen))
+
+# Python Length of a Array Example 1
+ArrayLenExample = [5, 12, 35, 22, 40, 5]
+print("\n", ArrayLenExample)
+
+print("Length of a Array = ", len(ArrayLenExample))
+
+# Python Array Length Example 2
+stringArrayLen = ['apple', 'cherry', 'kiwi', 'mango', 'orange', 'banana']
+print("\n", stringArrayLen)
+
+print("Length of a Array = ", len(stringArrayLen))
+
+# Python len function
+
+# Python Length of a Range
+rangeLen = range(4, 10)
+print("Python Range Length = ", len(rangeLen))
+
+# Python Length of a Range Example 2
+rangeLen = range(15, 109)
+print("Python Range Length = ", len(rangeLen))
+
+# Python len function  
+
+# Python Length of a Byte  
+byteLen = b'Python Tutorial'  
+print("Length of a Byte = ", len(byteLen)) 
