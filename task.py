3d7f159ecd3d5ed8650aa82e816c04e9d00b1064
vallarasu <100554866+vallaras@users.noreply.github.com>
2022-04-11 21:51:54 +0530

    Create task.py

---
+++ task.py
@@ -0,0 +1,37 @@
+
+str1="abcdd"
+lis=list(str1)
+res=[]
+for i in range(0,len(lis)):
+    for j in range(i+1,len(lis)):
+        if lis[i]==lis[j] and lis[i] not in res:
+            res.append(lis[i])
+
+print(res)
+
+
+str1="search the word in the sentance"
+lis=str1.split(" ")
+res=[]
+for i in range(0,len(lis)):
+    for j in range(i+1,len(lis)):
+        if lis[i]==lis[j] and lis[i] not in res:
+            res.append(lis[i])
+
+print(res)
+
+
+txt="python is a interpatter language, python quick to learn"
+a=txt.find('language')
+b=txt.capitalize()
+c=txt.split()
+d=txt.casefold()
+e=txt.center(150)
+f=txt.count('python',5,10)
+str=('hello,welcome to my world.')
+o=str.isascii()
+print(o)
+
+
+
+
