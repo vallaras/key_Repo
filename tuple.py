d788786cbd9d283d212ca350d2ef26cfe0008196
vallarasu <100554866+vallaras@users.noreply.github.com>
2022-04-17 19:03:53 +0530

    Create tuple.py

---
+++ tuple.py
@@ -0,0 +1,15 @@
+x = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+# Slicing the Tuple using two indexes
+a = x[2:6]
+print("Both Indexes = ", a)
+b = x[:6]
+print("No First Index = ", b)
+c = x[2:]
+print("No Second Index = ", c)
+d = x[:]
+print("No Indexes = ", d)
+e = x[-3:]
+print("Negative First Index = ", e)
+f = x[:-2]
+print("Negative Second Index = ", f)
